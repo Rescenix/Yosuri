7646a22ee77dd5f3f3699b2822334e1257b9d1c2
3478556810 <3478556810@qq.com>
2026-06-10 13:03:26 +0800

    android reading8
    Former-commit-id: 22a82a4857a45617ee4a27d5a71b6197bce58925
    Former-commit-id: 683898d2f40c276fa3f22a43f8a1aa4538d4c2b4

--- main-frontend/beneficial-belt/public/sw.js
+++ main-frontend/beneficial-belt/public/sw.js
@@ -2,11 +2,10 @@ const CACHE_NAME = 'shanxi-__BUILD_TIMESTAMP__';
 
 // 预缓存你的实际入口页面 + 核心资源
 const PRECACHE_URLS = [
-  '/shanxi-hut/',               // 应用主页面
-  '/shanxi-hut/index.html',     // 入口 HTML
-  '/manifest.webmanifest',      // 如果有
-  '/favicon.ico',
-  // 可以添加更多关键文件，如 '/_astro/xxx.css'
+  '/reading-hut/',
+  '/reading-hut/index.html',   // 如果存在
+  '/manifest.webmanifest',     // 如果有
+  // 可继续添加其他关键文件
 ];
 
 self.addEventListener('install', (event) => {
@@ -36,7 +35,7 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => {
       const network = fetch(event.request).then((response) => {
-        if (response.ok) {
+        if (response.ok && event.request.method === 'GET') {
           const clone = response.clone();
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
         }
